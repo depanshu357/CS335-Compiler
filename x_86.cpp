70f2163451a8e4bd3487f73c517f5d3f5cea36a6
ajmeraditya <adityaa21@iitk.ac.in>
2024-04-08 19:03:59 +0530

    x86 + implemented

--- x_86.cpp
+++ x_86.cpp
@@ -1,12 +1,31 @@
 #include<bits/stdc++.h>
 // #include "parser.y"
+#include "sym_table.cpp"
 using namespace std;
 
 // extern vector<vector<string>> instructions;
 // extern int tempCount;
 
-void print_x86_ins(vector<string> ins,ofstream &x86_file){
+int get_offset(string temp, sym_table* symbol_table){
+    if(temp[0]=='.'){
+        return stoi(temp.substr(2));
+    }
+    else if (temp[0] == '-' || temp[0]<='9' && temp[0]>='0')
+    {
+        return stoi(temp);
+    }else{
+        return get_offset_from_tbl(symbol_table, temp);
+    }
+}
+
+
+void print_x86_ins(vector<string> ins,ofstream &x86_file, sym_table* symbol_table){
     if(ins[0]=="3"){
+        int temp1=get_offset(ins[2],symbol_table);
+        int temp2=get_offset(ins[3],symbol_table);
+        int temp3=get_offset(ins[4],symbol_table);
+        
+        
         if(ins[1]=="+"){
             x86_file << "movq 0(%r1) %r2" << endl;
             x86_file << "movq 8(%r1) %r3" << endl;
@@ -21,7 +40,7 @@ void print_x86_ins(vector<string> ins,ofstream &x86_file){
 }
 
 
-void create_x86_code(vector<vector<string>> &instructions)
+void create_x86_code(vector<string> &instructions, sym_table* symbol_table)
 {
     //open file x86.s
     ofstream x86_file;
@@ -36,7 +55,7 @@ void create_x86_code(vector<vector<string>> &instructions)
 
 
     for(int i=0;i<instructions.size();i++){
-        print_x86_ins(instructions[i],x86_file);
+        print_x86_ins(instructions[i],x86_file,symbol_table);
         // for(int j=0;j<instructions[i].size();j++){
         //     x86_file << instructions[i][j] << endl;
         // }
