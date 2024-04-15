8c324c3abb629b8e0b518f95e93044268fff8eba
aryanmaurya383 <aryanms21@iitk.ac.in>
2024-04-15 18:22:06 +0530

    to check class func calls, associativity of mul div remain to check, array of string working

--- output/aryan.s
+++ output/aryan.s
@@ -6,39 +6,168 @@
         .text
 .global main
 
+#Test___init__:   
+Test___init__:
+pushq %rbp
+movq %rsp, %rbp
+subq $32, %rsp
+pushq %rbx
+pushq %rdi
+pushq %rsi
+pushq %r12
+pushq %r13
+pushq %r14
+pushq %r15
+
+#move8 0(%rbp) self 
+movq 0(%rbp), %r13
+movq %r13, -8(%rbp)
+
+#.t1 = self + 0
+movq -8(%rbp), %r13
+movq $0, %r14
+addq %r13, %r14
+movq %r14, -24(%rbp)
+
+#.t2 = 1
+movq $1, -32(%rbp)
+
+#move8 .t2 .t1 
+movq -32(%rbp), %r13
+movq %r13, -24(%rbp)
+
+#ret   
+popq %r15
+popq %r14
+popq %r13
+popq %r12
+popq %rsi
+popq %rdi
+popq %rbx
+addq $32, %rsp
+popq %rbp
+movq $8, %r13
+ret
+
+#Test_add:   
+Test_add:
+pushq %rbp
+movq %rsp, %rbp
+subq $56, %rsp
+pushq %rbx
+pushq %rdi
+pushq %rsi
+pushq %r12
+pushq %r13
+pushq %r14
+pushq %r15
+
+#move8 0(%rbp) self 
+movq 0(%rbp), %r13
+movq %r13, -8(%rbp)
+
+#move8 24(%rbp) b 
+movq 24(%rbp), %r13
+movq %r13, -16(%rbp)
+
+#.t3 = self + 8
+movq -8(%rbp), %r13
+movq $8, %r14
+addq %r13, %r14
+movq %r14, -40(%rbp)
+
+#.t4 = .t3 + b
+movq -40(%rbp), %r13
+movq -16(%rbp), %r14
+addq %r13, %r14
+movq %r14, -48(%rbp)
+
+#move8 .t4 x 
+movq -48(%rbp), %r13
+movq %r13, -56(%rbp)
+
+#move8 x %rax 
+movq -56(%rbp), %rax
+
+#ret   
+popq %r15
+popq %r14
+popq %r13
+popq %r12
+popq %rsi
+popq %rdi
+popq %rbx
+addq $56, %rsp
+popq %rbp
+movq $16, %r13
+ret
+
 #main:   
 main:
 pushq %rbp
 movq %rsp, %rbp
 subq $56, %rsp
 
-#.t1 = 8
-movq $8, -8(%rbp)
+##callnew   
+movq %rsp, -56(%rbp)
+shr $4, %rsp
+sub $1, %rsp
+shl $4, %rsp
 
-#.t2 = 4
-movq $4, -16(%rbp)
+#.t5 = alloc_mem(8) 
+movq %rsp, -56(%rbp)
+shr $4, %rsp
+sub $1, %rsp
+shl $4, %rsp
+movq $8, %rdi
+call malloc@plt
+movq -56(%rbp), %rsp
+movq %rax, -8(%rbp)
 
-#.t3 = 2
-movq $2, -24(%rbp)
+#push .t5  
+pushq -8(%rbp)
 
-#.t4 = .t2 - .t3
-movq -16(%rbp), %r13
-movq -24(%rbp), %r14
-subq %r14, %r13
+#call, Test___init__  
+call Test___init__
+addq %r13, %rsp
+movq -56(%rbp), %rsp
+
+#i = .t5
+movq -8(%rbp), %r13
+movq %r13, -16(%rbp)
+
+#.t6 = 0
+movq $0, -24(%rbp)
+
+#j = .t6
+movq -24(%rbp), %r13
 movq %r13, -32(%rbp)
 
-#.t5 = .t1 - .t4
-movq -8(%rbp), %r13
-movq -32(%rbp), %r14
-subq %r14, %r13
-movq %r13, -40(%rbp)
+#.t7 = 1
+movq $1, -40(%rbp)
 
-#x = .t5
-movq -40(%rbp), %r13
-movq %r13, -48(%rbp)
+##callnew   
+movq %rsp, -56(%rbp)
+shr $4, %rsp
+sub $1, %rsp
+shl $4, %rsp
 
-#print, x int 
-mov -48(%rbp), %rax
+#push .t7  
+pushq -40(%rbp)
+
+#push i  
+pushq -16(%rbp)
+
+#call, Test_add  
+call Test_add
+addq %r13, %rsp
+movq -56(%rbp), %rsp
+
+#move8 %rax j 
+movq %rax, -32(%rbp)
+
+#print, j int 
+mov -32(%rbp), %rax
 mov %rax, %rsi
 lea .note0(%rip), %rax
 mov %rax, %rdi
